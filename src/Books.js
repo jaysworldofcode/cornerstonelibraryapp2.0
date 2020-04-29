70c44be42685f3a3a56b59c9b5e7769eb120fc12
jlabueva <>
2020-04-29 20:14:18 +0800

    Fixes on path

--- src/Books.js
+++ src/Books.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import './css/global_style_sheet.css';
-import logo from './Book/testbooklet/cover.png';
+import logo from './Book/pagtulunan/cover.png';
 import ReactDOM from 'react-dom';
 import ViewBook from './ViewBook';
 
@@ -12,59 +12,55 @@ export default class Books extends React.Component {
         super(props);
 
         this.state = {
-            height: 0,
             isShouldAddMargin: false
           }
     };
-    ViewBook(){
-        ReactDOM.render(<ViewBook/>, document.getElementById('root'));
+    ViewBook(SetBookPath){
+        ReactDOM.render(<ViewBook BookPath={SetBookPath}/>, document.getElementById('root'));
     }
-    NewBook(
-        title,path,content,writer){
-          return    <Row id='book' onClick={this.ViewBook}>
-                        {/*style={{marginTop:this.state.height}}*/}
-                        <Col xs={4} md={2} xl={1} id='book-cover'>
-                           <img src={logo} alt="cover" className='center'/>
-                        </Col>
-                        <Col xs={8} md={10} xl={11}>
-                          <ul>
-                              <li id='book-header'> {title}</li>
-                              <li id='book-writer'>Author: {writer}</li>
-                              <li id='book-type'> 
-                                <span className='book-type-badge-custom'>Literature</span>
-                                <span className='book-type-badge-custom'>Novel</span>
-                              </li>
-                              <li id='book-view'>GET THIS BOOK</li>
-                          </ul>
-                        </Col>
-                    </Row>
+    NewBook(path){
+            const BookDetails = require('./Book/'+path+'/info.json');
+            return    <Row id='book' onClick={this.ViewBook.bind(this, path)}>
+                            {/*style={{marginTop:this.state.height}}*/}
+                            <Col xs={4} md={2} xl={1} id='book-cover'>
+                            <img src={require('./Book/'+path+'/cover.png')} alt="cover" className='center' style={{width:'80', height:'120px'}}/>
+                            </Col>
+                            <Col xs={8} md={10} xl={11}>
+                            <ul>
+                                <li id='book-header'> {BookDetails['BookTitle']}</li>
+                                <li id='book-writer'>Author: {BookDetails['Writer']}</li>
+                                <li id='book-type'> 
+                                    <span className='book-type-badge-custom'>Literature</span>
+                                    <span className='book-type-badge-custom'>Novel</span>
+                                </li>
+                                <li id='book-view'>GET THIS BOOK</li>
+                            </ul>
+                            </Col>
+                        </Row>
     }  
-    componentDidMount() {
-        const height = this.divElement.clientHeight;
-        this.setState({ height });
+    setBooks(){
+        const data = require('./Book/books.json');
+
+          data.map(function(item, i){
+            console.log(item);
+          }); 
+          console.log('tester');
     }
     render() {
+        const data = require('./Book/books.json');
         return (
             <div className='BooksContainer' style={{backgroundColor:'white'}}>
                 <div className='TopNavContainer' ref={ (divElement) => { this.divElement = divElement } }>
-                    {/* <Row>
-                        <Col>
-                            <p className='user'>Hello {this.props.username}!</p>
-                        </Col>
-                    </Row> */}
                     <Row style={{marginTop:'80px'}}>
                         <Col>
                             <p className='navTitle'>Bookshelf</p>
                         </Col>
                     </Row>
                 </div>
-                {this.NewBook('You are not alone','../assets/Images/banner.png','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu nisl, volutpat sed posuere eu.', 'Juan Dela Cruz')}
-                {this.NewBook('You are not alone','../assets/Images/banner.png','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu nisl, volutpat sed posuere eu.', 'Juan Dela Cruz')}
-                {this.NewBook('You are not alone','../assets/Images/banner.png','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu nisl, volutpat sed posuere eu.', 'Juan Dela Cruz')}
-                {this.NewBook('You are not alone','../assets/Images/banner.png','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu nisl, volutpat sed posuere eu.', 'Juan Dela Cruz')}
-                {this.NewBook('You are not alone','../assets/Images/banner.png','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu nisl, volutpat sed posuere eu.', 'Juan Dela Cruz')}
-                {this.NewBook('You are not alone','../assets/Images/banner.png','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu nisl, volutpat sed posuere eu.', 'Juan Dela Cruz')}
-                {this.NewBook('You are not alone','../assets/Images/banner.png','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu nisl, volutpat sed posuere eu.', 'Juan Dela Cruz')}
+                {this.setBooks()}
+                {data.map((value, index) => {
+                    return this.NewBook(value) 
+                })}
             </div>    
         );
     }
